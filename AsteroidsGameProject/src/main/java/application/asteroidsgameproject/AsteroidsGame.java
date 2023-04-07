47dde921e449074c8893a218b8f78fb311d2d751
paul-od24 <117522367+paul-od24@users.noreply.github.com>
2023-04-07 22:35:32 +0100

    +

--- AsteroidsGameProject/src/main/java/application/asteroidsgameproject/AsteroidsGame.java
+++ AsteroidsGameProject/src/main/java/application/asteroidsgameproject/AsteroidsGame.java
@@ -6,26 +6,27 @@ import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.*;
-import java.util.concurrent.atomic.AtomicBoolean;
-import javafx.scene.text.Text;
-import javafx.scene.layout.StackPane;
-import javafx.geometry.Pos;
-import javafx.geometry.Insets;
+import java.util.stream.Collectors;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.layout.VBox;
 
+//the larger game class that extends application
 public class AsteroidsGame extends Application {
+
+    //This variables that static means that they can be accessed using the class name rather than through an object of the class.
     public static int WIDTH;
     public static int HEIGHT;
-
-//    flag to check if a bullet has been fired
+    public static int level = 1;
+    Random rnd = new Random();
 
 
     @Override
@@ -37,144 +38,222 @@ public class AsteroidsGame extends Application {
 
         // Create the scene and canvas
         BorderPane root = new BorderPane();
-        Scene mainScene = new Scene(root, WIDTH, HEIGHT); // Set the size of the scene to the primary screen's dimensions
+        Scene mainScene = new Scene(root);
         mainStage.setScene(mainScene);
         mainStage.setTitle("Asteroids Game");
 
-// Create the score and lives text nodes
+        // Create the score and lives text nodes
+        root.setStyle("-fx-background-color: black;");
+
         Text scoreText = new Text("\nScore: 0");
         Text livesText = new Text("\nLives: ❤️ ❤️ ❤️");
         scoreText.setFill(Color.WHITE);
         livesText.setFill(Color.WHITE);
         scoreText.setFont(Font.font("Arial", FontWeight.BOLD, 24));
         livesText.setFont(Font.font("Arial", FontWeight.BOLD, 24));
+        VBox topBox = new VBox();
+        topBox.getChildren().addAll(scoreText, livesText);
+        root.setTop(topBox);
+        livesText.setLayoutY(AsteroidsGame.HEIGHT - 50);
 
-// Add the score and lives text nodes to the top center of the BorderPane
-        StackPane topCenterPane = new StackPane();
-        topCenterPane.getChildren().addAll(scoreText, livesText);
-        topCenterPane.setAlignment(Pos.TOP_LEFT);
-        topCenterPane.setMargin(livesText, new Insets(30, 0, 0, 0)); // add a 20-pixel gap between the two Text nodes
-        root.setTop(topCenterPane);
-        topCenterPane.setStyle("-fx-background-color: black;");
 
         Canvas canvas = new Canvas(WIDTH, HEIGHT);
         GraphicsContext gc = canvas.getGraphicsContext2D();
         root.setCenter(canvas);
-
-        // Set the background color of the game window
+//fill screen color
         gc.setFill(Color.BLACK);
         gc.fillRect(0, 0, WIDTH, HEIGHT);
-//player ship
+//initialising playership
         PlayerShip playership = new PlayerShip(WIDTH/2, HEIGHT/2);
-//        list of asteroids
-        List<Asteroids> asteroids = new ArrayList<>();
-        Random rnd = new Random();
-
-// Generate large asteroids
-        for (int i = 0; i < 5; i++) {
-            LargeAsteroid asteroid = new LargeAsteroid(rnd.nextInt(WIDTH), rnd.nextInt(HEIGHT));
-            asteroids.add(asteroid);
-        }
-
-// Generate medium asteroids
-        for (int i = 0; i < 5; i++) {
-            MediumAsteroid asteroid = new MediumAsteroid(rnd.nextInt(WIDTH), rnd.nextInt(HEIGHT));
-            asteroids.add(asteroid);
-        }
-
-// Generate small asteroids
-        for (int i = 0; i < 5; i++) {
-            SmallAsteroid asteroid = new SmallAsteroid(rnd.nextInt(WIDTH), rnd.nextInt(HEIGHT));
-            asteroids.add(asteroid);
-        }
-
-        ArrayList<Bullet> bulletList = new ArrayList<>();
-
-
+//        add player to game window
         root.getChildren().add(playership.getGameCharacter());
+
+//initialising a list of asterioids
+        ArrayList<Asteroids> asteroids = new ArrayList<>();
+
+//generating a random position to the first large asteroids
+        LargeAsteroid asteroid_one = new LargeAsteroid(rnd.nextInt(WIDTH), rnd.nextInt(HEIGHT));
+//add large asteroid to asteroid list
+        asteroids.add(asteroid_one);
+//        add asteroid to game window
         asteroids.forEach(asteroid -> root.getChildren().add(asteroid.getGameCharacter()));
+// initialising a list of bullests
+        ArrayList<Bullet> bulletList = new ArrayList<>();
+        // handles continuous inputs (as long as key is pressed)
+        ArrayList<String> keyPressedList = new ArrayList<>();
+
+        // handles discrete inputs (one per key press)
+        ArrayList<String> keyJustPressedList = new ArrayList<>();
+//captures the pressed key and adds it to two ArrayLists: "keyPressedList" and "keyJustPressedList".
+        mainScene.setOnKeyPressed(
+                (KeyEvent event) -> {
+                    String keyName = event.getCode().toString();
+                    //avoid adding duplicates to list
+                    if (!keyPressedList.contains(keyName)) {
+                        keyPressedList.add(keyName);
+                        keyJustPressedList.add(keyName);
+                    }
+                }
+        );
+//captures the released key and removes it from the "keyPressedList" ArrayList if it was already in the list.
+        mainScene.setOnKeyReleased(
+                (KeyEvent event) -> {
+                    String keyName = event.getCode().toString();
+                    if (keyPressedList.contains(keyName)) {
+                        keyPressedList.remove(keyName);
+                    }
+                }
+        );
 
 
-//    keeping a record of the pressed keys using a hash table, if the key is pressed the boolean returns true
-        Map<KeyCode, Boolean> pressedKeys = new HashMap<>();
-        mainScene.setOnKeyPressed(event -> {
-            pressedKeys.put(event.getCode(), Boolean.TRUE);
-        });
-
-        mainScene.setOnKeyReleased(event -> {
-            pressedKeys.put(event.getCode(), Boolean.FALSE);
-        });
-
-
-        new AnimationTimer() {
-            AtomicBoolean hasFiredBullet = new AtomicBoolean(false);
+        AnimationTimer game = new AnimationTimer() {
 
             @Override
             public void handle(long now) {
-                if (pressedKeys.getOrDefault(KeyCode.LEFT, false)) {
+
+                if (keyPressedList.contains("LEFT")) {
                     playership.turnLeft();
                 }
 
-                if (pressedKeys.getOrDefault(KeyCode.RIGHT, false)) {
+                if (keyPressedList.contains("RIGHT")) {
                     playership.turnRight();
                 }
 
 
-                if(pressedKeys.getOrDefault(KeyCode.UP, false)) {
-                    playership.accelerate(0.05);
+                if (keyPressedList.contains("UP")) {
+                    playership.accelerate(0.07);
                 }
 
-                if (pressedKeys.getOrDefault(KeyCode.SHIFT, false)) {
+                if (keyPressedList.contains("SHIFT")) {
                     playership.hyperJump();
+//checking if player collides with asteroid
+                    asteroids.forEach(asteroid -> {
+                        if (playership.collision(asteroid)) {
+                            playership.hyperJump();
+                        }
+                    });
                 }
 
-                if (hasFiredBullet.get() == false && pressedKeys.getOrDefault(KeyCode.SPACE, false) && bulletList.size() < 10) {
+                if (keyJustPressedList.contains("SPACE") ) {
                     // user can fire a bullet
 //                    System.out.println("bullet has been shot");
                     Bullet bullet = new Bullet((int) playership.getGameCharacter().getTranslateX(), (int) playership.getGameCharacter().getTranslateY());
                     bullet.getGameCharacter().setRotate(playership.getGameCharacter().getRotate());
                     bulletList.add(bullet);
 
-                    bullet.accelerate(0.05);
+                    bullet.accelerate(0.3); // sped up bullet speed
                     bullet.setMovement(bullet.getMovement().normalize().multiply(3));
 
                     root.getChildren().add(bullet.getGameCharacter());
 
-                    // set hasFiredBullet to true to indicate that the user has fired a bullet
-                    hasFiredBullet.set(true);
                 }
+//                clears the keyPressesdlist ensures smooth pressing of bullets
+                keyJustPressedList.clear();
 
-
+//player moves
                 playership.move();
-
-//                getting the bullets from the the bullet list ensuring thet dont stay more than 5 seconds on the screen
+//asteroids move
                 asteroids.forEach(asteroid -> asteroid.move());
 
+// getting the bullets from the bullet list ensuring they don't stay more than 5 seconds on the screen
+
                 for (int n = 0; n < bulletList.size(); n++) {
-
-                    GameCharacters bullet = bulletList.get(n);
-
+                    Bullet bullet = bulletList.get(n);
                     bullet.move();
-                    bullet.update(1/60.0);
-                    if (bullet.elapseTimeSeconds > 2) {
+                    bullet.update(1 / 60.0);
+                    if (bullet.elapseTimeSeconds > 5) {
                         bulletList.remove(n);
                         root.getChildren().remove(bullet.getGameCharacter());
                     }
                 }
 
+//               collision detection
+                // disappear bullet when it hit
+                List<Bullet> bulletToRemove = bulletList.stream().filter(bullet -> {
+                    List<Asteroids> collisions = asteroids.stream()
+                            .filter(asteroid -> asteroid.collision(bullet))
+                            .collect(Collectors.toList());
+
+                    if(collisions.isEmpty()){
+                        return false;
+                    }
+
+                    collisions.stream().forEach(collided -> {
+                        asteroids.remove(collided);
+                        root.getChildren().remove(collided.getGameCharacter());
+
+                        if (collided instanceof LargeAsteroid) {
+                            for (int i = 0; i < 2; i++) {
+                                MediumAsteroid asteroidM = new MediumAsteroid((int) collided.getGameCharacter().getTranslateX(), (int) collided.getGameCharacter().getTranslateY());
+                                asteroids.add(asteroidM);
+                                root.getChildren().add(asteroidM.getGameCharacter());
+                                asteroids.forEach(asteroid -> asteroid.move());
+
+                            }
+                        } else if (collided instanceof MediumAsteroid) {
+                            for (int i = 0; i < 2; i++) {
+                                SmallAsteroid asteroidS = new SmallAsteroid((int) collided.getGameCharacter().getTranslateX(), (int) collided.getGameCharacter().getTranslateY());
+                                asteroids.add(asteroidS);
+                                root.getChildren().add(asteroidS.getGameCharacter());
+                                asteroids.forEach(asteroid -> asteroid.move());
+
+                            }
+                        } else if (collided instanceof SmallAsteroid) {
+                            asteroids.remove(collided);
+                        }
+
+                        asteroids.forEach(asteroid -> asteroid.move());
+                    });
+
+                    return true;
+                }).collect(Collectors.toList());
+
+//remove bullets once they have hit the asteroid
+                bulletToRemove.forEach(bullet -> {
+                    root.getChildren().remove(bullet.getGameCharacter());
+                    bulletList.remove(bullet);
+
+// Check if there are any asteroids left on the screen
+
+                    if (asteroids.isEmpty()) {
+                        // Increase the level and add more large asteroids
+                        level++;
+                        System.out.print("Level" + level);
+
+
+                        int numLargeAsteroids = (int) Math.ceil(level * 1); // Formula to calculate number of large asteroids
+                        for (int i = 0; i < numLargeAsteroids; i++) {
+                            System.out.println("NUMBER OF ASTEROIDS" + numLargeAsteroids);
+////                            // generating a random position to the
+                            Random rnd = new Random();
+//                            // Generate a the first large asteroids
+                            LargeAsteroid asteroid_to_add_next_level = new LargeAsteroid(rnd.nextInt(WIDTH), rnd.nextInt(HEIGHT));
+                            asteroids.add(asteroid_to_add_next_level);
+                            root.getChildren().add(asteroid_to_add_next_level.getGameCharacter());
+                            asteroids.forEach(asteroid -> asteroid.move());
+                        }
+                    }
+
+                });
+
+//checking if player collides with asteroid
                 asteroids.forEach(asteroid -> {
                     if (playership.collision(asteroid)) {
-//                         stop();
+//                        stop();
+//                        System.out.println("You die!");
                     }
+
                 });
+
+
 
             }
 
 
 
 
-
-        }.start();
+        };
+        game.start();
         mainStage.show();
 
     }
--- AsteroidsGameProject/src/main/java/application/asteroidsgameproject/MainMenu.java
+++ AsteroidsGameProject/src/main/java/application/asteroidsgameproject/MainMenu.java
@@ -16,6 +16,9 @@ import javafx.scene.layout.BackgroundRepeat;
 import javafx.scene.layout.BackgroundPosition;
 import javafx.scene.layout.BackgroundSize;
 import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import java.io.IOException;
+import javafx.geometry.Insets;
 
 public class MainMenu extends Application {
     public static int WIDTH;
@@ -24,101 +27,103 @@ public class MainMenu extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        // Get the dimensions of the primary screen
-        WIDTH = (int) PRIMARY_SCREEN_BOUNDS.getWidth();
-        HEIGHT = (int) PRIMARY_SCREEN_BOUNDS.getHeight();
-        // Create the main menu welcome screen
-        VBox welcomeLayout = createWelcomeLayout(primaryStage); // Create a VBox layout for the welcome screen
-        Scene welcomeScene = new Scene(welcomeLayout, WIDTH, HEIGHT); // Create a scene with the welcome layout and size
+        // Get the screen dimensions
+        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+        int screenWidth = (int) primaryScreenBounds.getWidth();
+        int screenHeight = (int) primaryScreenBounds.getHeight();
 
-        // Set the main menu scene
-        primaryStage.setScene(welcomeScene); // Set the welcome screen as the main menu scene
-        primaryStage.setTitle("Asteroids Game"); // Set the title of the stage
-        primaryStage.show(); // Show the stage
-    }
+        // Set the window dimensions
+        WIDTH = screenWidth;
+        HEIGHT = screenHeight;
 
-    private VBox createWelcomeLayout(Stage primaryStage) {
-        // Create the VBox layout for the welcome screen
-        VBox welcomeLayout = new VBox(20);
 
-        // Create the title label
-        Label titleLabel = new Label("Asteroids");
-        titleLabel.setFont(Font.font(50)); // Set the font size of the title label
-        titleLabel.setStyle("-fx-text-fill: white;"); // Set the text color of the title label to white
+        // Create the main menu scene
+        BorderPane root = new BorderPane();
 
-        // Create the "Play Game" button
-        Button playButton = new Button("Play Game");
-        playButton.setFont(Font.font(24)); // Set the font size of the play button
+        // Set the background image of the root pane
+        Image backgroundImage = new Image("/spaceBackground.png");
+        BackgroundSize backgroundSize = new BackgroundSize(screenWidth, screenHeight, false, false, true, false);
+        BackgroundImage background = new BackgroundImage(backgroundImage, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        root.setBackground(new Background(background));
+
+        Scene scene = new Scene(root, screenWidth, screenHeight);
+
+        // Create a button to launch the game
+        Button playButton = new Button("Play Asteroids");
+        playButton.setFont(Font.font(30));
+        playButton.setPrefWidth(300);
         playButton.setOnAction(e -> {
-            // Launch the AsteroidsGame
             try {
-                AsteroidsGame asteroidsGame = new AsteroidsGame();
-                asteroidsGame.start(primaryStage);
+                // Launch the game
+                AsteroidsGame game = new AsteroidsGame();
+                game.start(new Stage());
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
         });
 
-        // Load the background image
-        Image backgroundImage = new Image(getClass().getResource("/spaceBackground.png").toExternalForm());
+        // Create a button to view high scores
+        Button highScoresButton = new Button("High Scores");
+        highScoresButton.setFont(Font.font(30));
+        highScoresButton.setPrefWidth(300);
 
-        // Create the background
-        BackgroundSize backgroundSize = new BackgroundSize(WIDTH, HEIGHT, true, true, true, true);
-        BackgroundImage backgroundImg = new BackgroundImage(backgroundImage, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, backgroundSize);
-        Background background = new Background(backgroundImg);
-
-        // Set the background of the VBox
-        welcomeLayout.setBackground(background);
-
-        // Create the "High Scores" button
-        Button highScoresButton = new Button("Hall of Fame - High Scores");
-        highScoresButton.setFont(Font.font(24)); // Set the font size of the high scores button
         highScoresButton.setOnAction(e -> {
-            // TODO: Implement the high scores action
+            // Show the high scores screen
+            System.out.println("TODO: Show high scores screen");
         });
 
-        // Create the "Controls" button
+        // Create a button to view controls
         Button controlsButton = new Button("Controls");
-        controlsButton.setFont(Font.font(24)); // Set the font size of the controls button
-        controlsButton.setOnAction(e -> {
-            // Create the controls layout
-            VBox controlsLayout = createControlsLayout(primaryStage); // Pass backgroundImg as a parameter
-            Scene controlsScene = new Scene(controlsLayout, WIDTH, HEIGHT); // Create a scene with the controls layout and size
+        controlsButton.setFont(Font.font(30));
+        controlsButton.setPrefWidth(300);
 
-            // Set the controls scene
-            primaryStage.setScene(controlsScene); // Set the controls screen as the main menu scene
+        controlsButton.setOnAction(e -> {
+            // Show the controls screen
+            VBox controlsLayout = createControlsLayout(primaryStage, scene);
+            Scene controlsScene = new Scene(controlsLayout, WIDTH, HEIGHT);
+            primaryStage.setScene(controlsScene);
         });
 
-        // Add the title label and buttons to the layout
-        welcomeLayout.getChildren().addAll(titleLabel, playButton, highScoresButton, controlsButton);
-        welcomeLayout.setAlignment(Pos.CENTER); // Set the alignment of the layout to center
-        return welcomeLayout; // Return the layout
+        // Add the buttons to the main menu
+        VBox buttonBox = new VBox(10);
+        buttonBox.setAlignment(Pos.CENTER);
+        buttonBox.getChildren().addAll(playButton, highScoresButton, controlsButton);
+        root.setCenter(buttonBox);
+
+        // Show the main menu
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Asteroids");
+        primaryStage.show();
     }
 
-    private VBox createControlsLayout(Stage primaryStage) {
+    private VBox createControlsLayout(Stage primaryStage, Scene scene) {
+        // Get the screen dimensions
+        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+        int screenWidth = (int) primaryScreenBounds.getWidth();
+        int screenHeight = (int) primaryScreenBounds.getHeight();
+
+        // Create the VBox layout for the controls screen
+        VBox controlsLayout = new VBox(20);
+        controlsLayout.setAlignment(Pos.CENTER);
+
+        // Set the background image of the controls layout
+        Image backgroundImage = new Image("/spaceBackground.png");
+        BackgroundSize backgroundSize = new BackgroundSize(screenWidth, screenHeight, false, false, true, false);
+        BackgroundImage background = new BackgroundImage(backgroundImage, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        controlsLayout.setBackground(new Background(background));
+
         // Create the "Back" button
         Button backButton = new Button("Back");
-        backButton.setFont(Font.font(24)); // Set the font size of the back button
+        backButton.setFont(Font.font(24));
         backButton.setOnAction(e -> {
             // Go back to the main menu
-            VBox welcomeLayout = createWelcomeLayout(primaryStage); // Create a VBox layout for the welcome screen
-            Scene welcomeScene = new Scene(welcomeLayout, WIDTH, HEIGHT); // Create a scene with the welcome layout and size
-
-            primaryStage.setScene(welcomeScene); // Set the welcome screen as the main menu scene
+            primaryStage.setScene(scene);
         });
 
         // Create the controls label
         Label controlsLabel = new Label("Controls:");
-        controlsLabel.setFont(Font.font(20)); // Set the font size of the controls label
-        controlsLabel.setStyle("-fx-text-fill: white;"); // Set the text color of the controls label to white
-
-        // Load the background image
-        Image backgroundControls = new Image(getClass().getResource("/spaceBackground.png").toExternalForm());
-
-        // Create the background
-        BackgroundSize backgroundSize = new BackgroundSize(WIDTH, HEIGHT, true, true, true, true);
-        BackgroundImage backgroundImg = new BackgroundImage(backgroundControls, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, backgroundSize);
-        Background background = new Background(backgroundImg);
+        controlsLabel.setFont(Font.font(20));
+        controlsLabel.setStyle("-fx-text-fill: white;");
 
         // Create the controls text
         Label controlsText = new Label("Use the arrow keys to move the ship.\nThe up arrow key moves the ship forward, \nThe left and right arrow keys rotate the ship\n" +
@@ -133,18 +138,13 @@ public class MainMenu extends Application {
                 "Remember to have fun and good luck!");
         controlsText.setStyle("-fx-text-fill: white;"); // Set the text color of the controls text to white
 
-        // Add the controls label, text, and back button to the layout
-        VBox layout = new VBox(20, controlsLabel, controlsText, backButton);
-        layout.setAlignment(Pos.CENTER);
+        // Add the controls label, text, and buttons to the layout
+        controlsLayout.getChildren().addAll(controlsLabel, controlsText, backButton);
 
-        // Set the background of the VBox
-        layout.setBackground(background);
-
-        Scene controlsScene = new Scene(layout, WIDTH, HEIGHT);
-        primaryStage.setScene(controlsScene); // Set the controls scene as the main menu scene
-
-        return layout;
+        return controlsLayout;
     }
+
+
     public static void main(String[] args) {
         launch(args);
     }
