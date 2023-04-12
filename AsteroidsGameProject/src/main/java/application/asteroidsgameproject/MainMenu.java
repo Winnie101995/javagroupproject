dc3aa43e336af908d5d555374c22493ea7747fad
paul-od24 <117522367+paul-od24@users.noreply.github.com>
2023-04-12 18:02:01 +0100

    controls class & background fix

--- AsteroidsGameProject/src/main/java/application/asteroidsgameproject/MainMenu.java
+++ AsteroidsGameProject/src/main/java/application/asteroidsgameproject/MainMenu.java
@@ -16,37 +16,46 @@ import javafx.scene.layout.BackgroundRepeat;
 import javafx.scene.layout.BackgroundPosition;
 import javafx.scene.layout.BackgroundSize;
 import javafx.scene.image.Image;
-import javafx.scene.layout.BorderPane;
-import java.io.IOException;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.FontWeight;
+import javafx.scene.paint.Color;
 import javafx.geometry.Insets;
 
 public class MainMenu extends Application {
     public static int WIDTH;
     public static int HEIGHT;
-    public static final Rectangle2D PRIMARY_SCREEN_BOUNDS = Screen.getPrimary().getVisualBounds();
+    //public static final Rectangle2D PRIMARY_SCREEN_BOUNDS = Screen.getPrimary().getVisualBounds();
 
     @Override
     public void start(Stage primaryStage) {
         // Get the screen dimensions
-        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
-        int screenWidth = (int) primaryScreenBounds.getWidth();
-        int screenHeight = (int) primaryScreenBounds.getHeight();
-
-        // Set the window dimensions
-        WIDTH = screenWidth;
-        HEIGHT = screenHeight;
-
-
-        // Create the main menu scene
-        BorderPane root = new BorderPane();
+        //Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+        Screen primaryScreen = Screen.getPrimary();
+        Rectangle2D bounds = primaryScreen.getBounds();
+        WIDTH = (int) bounds.getWidth();
+        HEIGHT = (int) bounds.getHeight();
 
         // Set the background image of the root pane
         Image backgroundImage = new Image("/spaceBackground.png");
-        BackgroundSize backgroundSize = new BackgroundSize(screenWidth, screenHeight, false, false, true, false);
+        BackgroundSize backgroundSize = new BackgroundSize(WIDTH, HEIGHT, true, true, true, true);
         BackgroundImage background = new BackgroundImage(backgroundImage, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+
+        // Create the main menu scene
+        StackPane root = new StackPane();
         root.setBackground(new Background(background));
 
-        Scene scene = new Scene(root, screenWidth, screenHeight);
+        Scene scene = new Scene(root, WIDTH, HEIGHT);
+
+        // Set the stage title
+        primaryStage.setTitle("Asteroids Game Main Menu");
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        // Create a label for the title
+        Label titleLabel = new Label("ASTEROIDS");
+        titleLabel.setFont(Font.font("Verdana", FontWeight.BOLD, 50));
+        titleLabel.setTextFill(Color.WHITE);
 
         // Create a button to launch the game
         Button playButton = new Button("Play Asteroids");
@@ -79,72 +88,22 @@ public class MainMenu extends Application {
 
         controlsButton.setOnAction(e -> {
             // Show the controls screen
-            VBox controlsLayout = createControlsLayout(primaryStage, scene);
-            Scene controlsScene = new Scene(controlsLayout, WIDTH, HEIGHT);
+            ControlsScreen controlsScreen = new ControlsScreen(scene, primaryStage);
+            Scene controlsScene = controlsScreen.createScene();
             primaryStage.setScene(controlsScene);
         });
-
-        // Add the buttons to the main menu
-        VBox buttonBox = new VBox(10);
-        buttonBox.setAlignment(Pos.CENTER);
-        buttonBox.getChildren().addAll(playButton, highScoresButton, controlsButton);
-        root.setCenter(buttonBox);
 
         // Show the main menu
         primaryStage.setScene(scene);
         primaryStage.setTitle("Asteroids");
         primaryStage.show();
+        // Add the title label and button to the main menu
+        VBox mainMenuLayout = new VBox(50);
+        mainMenuLayout.setAlignment(Pos.CENTER);
+        mainMenuLayout.setPadding(new Insets(0, 0, 0, 0));
+        mainMenuLayout.getChildren().addAll(titleLabel,playButton, highScoresButton, controlsButton);
+        root.getChildren().add(mainMenuLayout);
     }
-
-    private VBox createControlsLayout(Stage primaryStage, Scene scene) {
-        // Get the screen dimensions
-        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
-        int screenWidth = (int) primaryScreenBounds.getWidth();
-        int screenHeight = (int) primaryScreenBounds.getHeight();
-
-        // Create the VBox layout for the controls screen
-        VBox controlsLayout = new VBox(20);
-        controlsLayout.setAlignment(Pos.CENTER);
-
-        // Set the background image of the controls layout
-        Image backgroundImage = new Image("/spaceBackground.png");
-        BackgroundSize backgroundSize = new BackgroundSize(screenWidth, screenHeight, false, false, true, false);
-        BackgroundImage background = new BackgroundImage(backgroundImage, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
-        controlsLayout.setBackground(new Background(background));
-
-        // Create the "Back" button
-        Button backButton = new Button("Back");
-        backButton.setFont(Font.font(24));
-        backButton.setOnAction(e -> {
-            // Go back to the main menu
-            primaryStage.setScene(scene);
-        });
-
-        // Create the controls label
-        Label controlsLabel = new Label("Controls:");
-        controlsLabel.setFont(Font.font(20));
-        controlsLabel.setStyle("-fx-text-fill: white;");
-
-        // Create the controls text
-        Label controlsText = new Label("Use the arrow keys to move the ship.\nThe up arrow key moves the ship forward, \nThe left and right arrow keys rotate the ship\n" +
-                "\n" +
-                "Press the space bar to shoot lasers"
-                +"\nAvoid colliding with asteroids and enemy ships.\nColliding with them will destroy the player's ship.\n" +
-                "\n" +
-                "Destroy all asteroids and enemy ships to complete the level.\n" +
-                "\n" +
-                "The player has a limited number of lives. \nLosing all lives will result in a game over.\n" +
-                "\n" +
-                "Remember to have fun and good luck!");
-        controlsText.setStyle("-fx-text-fill: white;"); // Set the text color of the controls text to white
-
-        // Add the controls label, text, and buttons to the layout
-        controlsLayout.getChildren().addAll(controlsLabel, controlsText, backButton);
-
-        return controlsLayout;
-    }
-
-
     public static void main(String[] args) {
         launch(args);
     }
